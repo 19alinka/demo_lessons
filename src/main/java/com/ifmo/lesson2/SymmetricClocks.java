547552e36cc0ffec89f6179b3997a405a33c860c
19alinka <alinka030398@mail.ru>
2021-10-18 00:06:43 +0300

    SymmetricClocks

--- src/main/java/com/ifmo/lesson2/SymmetricClocks.java
+++ src/main/java/com/ifmo/lesson2/SymmetricClocks.java
@@ -14,15 +14,15 @@ public class SymmetricClocks {
     public static int symmetricTimes() {
         // TODO implement
         int sum = 0;
-        for (int e = 0; e <= 24; e++) {
-            int a = e / 10;
-            int x = e % 10;
-            int s = a + x;
-            for (int r = 0; r <= 60; r++) {
-                int q = r / 10;
-                int p = r % 10;
-                int d = q + p;
-                if (s == d) sum++;
+        for (int e = 0; e <= 23; e++) {
+            char a = (char) (e / 10);
+            char x = (char) (e % 10);
+            String s = "" + a + x;
+            for (int r = 0; r <= 59; r++) {
+                char q = (char) (r / 10);
+                char z = (char) (r % 10);
+                String d = "" + z + q;
+                    if (s.equals(d)) sum++;
             }
         }
             return sum;
